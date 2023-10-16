0187ce136fd9d0c8777c94ff07245b25a8c2125c
drnschv <adrian2005@gmail.com>
2023-10-16 14:54:02 +0000

    Loclization commit

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,7 +1,11 @@
 using hc450.officesupplies as officesupplies from '../db/schema';
 
 service CatalogService {
- //@odata.draft.enabled :true
- entity Products as projection on officesupplies.Products;
- entity Suppliers as projection on officesupplies.Suppliers;
+    @odata.draft.enabled :true
+    entity Products  as projection on officesupplies.Products;
+    
+    entity Suppliers as projection on officesupplies.Suppliers;
+
+    function get_supplier_info() returns array of Suppliers;
+
 };
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,9 +1,9 @@
 const cds = require('@sap/cds');
 
-module.exports = cds.service.impl( function () {
-    const {Products} = this.entities();
+module.exports = cds.service.impl(function () {
+    const { Products } = this.entities();
 
-    this.after('each', 'Products', row =>{
+    this.after('each', 'Products', row => {
         //console.log('Read Product ID: ${row.ID}')
         //console.log(`Read Product: ${row.ID}`);
         console.log('READ --->>>', row.ID);
@@ -17,6 +17,25 @@ module.exports = cds.service.impl( function () {
         });
 
 
+    });
+
+    this.on('get_supplier_info', async () => {
+        try {
+            const db = await cds.connect.to('db');
+            const dbClass = require("sap-hdbext-promisfied");
+            let dbConn = new dbClass(await dbClass.createConnection(db.options.credentials));
+            const hdbext = require("@sap/hdbext");
+            const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'get_supplier_info');
+            const output = await dbConn.callProcedurePromisified(sp, []);
+            console.log(output.results);
+            return output.results;
+
+        }
+        catch (error) {
+            console.error(error);
+            return;
+
+        }
     });
 
 
